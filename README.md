ccdaacacc57293a2bfda48b1cb33d3594c907b08
infamousSs <locdaohuuit@outlook.com>
2022-02-06 22:07:12 -0800

    [dhuloc] Getting started conan

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # cassadian
+
+## Requirements
+
+1. Cmake >= 3.14
+2. gcc 11
+3. conan
+
+## How to run
+
+1. Change compiler in run.sh </br>
+	1.1. C_COMPILER=/usr/local/bin/gcc-11 </br>
+    1.2. CPP_COMPILER=/usr/local/bin/g++-11 </br>
+2. Run <code>sh run.sh debug</code>
--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
-using namespace std;
+#include "Poco/MD5Engine.h"
+#include "Poco/DigestStream.h"
 
-int main() {
-    cout << "Hello world";
+#include <iostream>
+
+int main(){
+    Poco::MD5Engine md5;
+    Poco::DigestOutputStream ds(md5);
+    ds << "Hello, World";
+    ds.close();
+    std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
     return 0;
 }
